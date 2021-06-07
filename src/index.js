de687ea27da408a61374a94efc956589be3ccbbf
Pierina Montalva <pierinamfatur@gmail.com>
2021-06-07 14:01:58 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,141 @@
 import validator from './validator.js';
+//console.log(validator); 
 
-console.log(validator);
+/**Función de botón para iniciar */
+document.getElementById('startbtn').addEventListener('click', start);
+function start() {
+    const startContent = document.getElementsByClassName('home-container');
+    const infoContent = document.getElementsByClassName('info-container');
+
+    for (let i = 0; i < startContent.length; i++) {
+        startContent[i].style.display = 'none';
+    }
+
+    for (let i = 0; i < infoContent.length; i++) {
+        infoContent[i].style.display = 'block';
+    }
+}
+
+/**Función de botón de información */
+document.getElementById('infobtn').addEventListener('click', showInfo);
+function showInfo() {
+    const showInfoContent = document.getElementsByClassName('show-info-container');
+    const infoContent = document.getElementsByClassName('info-container');
+    const showHeader = document.getElementsByClassName('header');
+    const showFooter = document.getElementsByClassName('footer');
+
+    for (let i = 0; i < showInfoContent.length; i++) {
+        showInfoContent[i].style.display = 'block';
+    }
+    for (let i = 0; i < infoContent.length; i++) {
+        infoContent[i].style.display = 'none';
+    }
+    for (let i = 0; i < showHeader.length; i++) {
+        showHeader[i].style.display = 'block';
+    }
+    for (let i = 0; i < showFooter.length; i++) {
+        showFooter[i].style.display = 'block';
+    }
+}
+
+/**Función de botón para is a validar tarjeta */
+document.getElementById('validbtn').addEventListener('click', showCard);
+function showCard() {
+    const infoContainer = document.getElementsByClassName('info-container');
+    const showCardContent = document.getElementsByClassName('card-valid-container');
+    const showHeader = document.getElementsByClassName('header');
+    const showFooter = document.getElementsByClassName('footer');
+    
+    for (let i = 0; i < infoContainer.length; i++) {
+        infoContainer[i].style.display = 'none';
+    }
+    for (let i = 0; i < showCardContent.length; i++) {
+        showCardContent[i].style.display = 'block';
+    }
+    for (let i = 0; i < showHeader.length; i++) {
+        showHeader[i].style.display = 'block';
+    }
+    for (let i = 0; i < showFooter.length; i++) {
+        showFooter[i].style.display = 'block';
+    }
+}
+
+/**Función de botón para ir a validar tarjeta */
+document.getElementById('validbutton').addEventListener('click', displayCard);
+function displayCard() {
+    const showInfoContainer = document.getElementsByClassName('show-info-container');
+    const showCardContent = document.getElementsByClassName('card-valid-container');
+    const showHeader = document.getElementsByClassName('header');
+    const showFooter = document.getElementsByClassName('footer');
+    
+    for (let i = 0; i < showInfoContainer.length; i++) {
+        showInfoContainer[i].style.display = 'none';
+    }
+    for (let i = 0; i < showCardContent.length; i++) {
+        showCardContent[i].style.display = 'block';
+    }
+    for (let i = 0; i < showHeader.length; i++) {
+        showHeader[i].style.display = 'block';
+    }
+    for (let i = 0; i < showFooter.length; i++) {
+        showFooter[i].style.display = 'block';
+    }
+}
+
+// PERMITIR SOLO NÚMEROS EN EL INPUT 
+document.getElementById('inputCard').addEventListener('keypress', convert);
+
+function convert(e) { 
+   
+    if(!justNumbers(e)) {
+        e.preventDefault();
+    }
+
+    function justNumbers(e) {
+        
+        let key = e.keyCode || e.which;
+        
+        if(key >= 48 && key <= 57) {
+            return true;
+        } else {
+            alert('Ingresar solo números');
+            return false;
+        }
+    }
+}
+
+document.getElementById('cardbtn').addEventListener('click', getNumber);
+function getNumber() {
+    //(1) Obtener input y colocarlo en reversa
+    const inputCard = document.getElementById('inputCard').value;
+    const reverseInput = inputCard.split('').reverse();
+    
+    validator.isValid(reverseInput);
+  
+    //(2) Alert para que no dejen el input vacío
+    if(inputCard.length === 0) {
+        alert('No puedes dejar este campo vacío');
+    } 
+    
+    //(3) Implementar la multiplicación solo a los números pares 
+    //.match(/\d+/g)
+    let newArray = reverseInput.map(even => {
+        if(even % 2 == 0) {
+            return (even*2);
+        } else {
+            return even;
+        }
+        
+    });
+    console.log(newArray);
+    
+}
+
+   /**
+     var numbers = [1, 5, 10, 15];
+    var doubles = numbers.map(function(x) {
+    return x * 2;
+    });
+    // doubles is now [2, 10, 20, 30]
+    // numbers is still [1, 5, 10, 15]
+    */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
-const validator = {
-  // ...
-};
+ /**validator.isValid(creditCardNumber): creditCardNumber es un string con el número de tarjeta que se va a verificar. Esta función debe retornar un boolean dependiendo si es válida de acuerdo al algoritmo de Luhn. 
+ 
+  validator.maskify(creditCardNumber): creditCardNumber es un string con el número de tarjeta y esta función debe retornar un string donde todos menos los últimos cuatro caracteres sean reemplazados por un numeral (#) o 🐱. Esta función deberá siempre mantener los últimos cuatro caracteres intactos, aún cuando el string sea de menor longitud. */
 
-export default validator;
+const validator = {
+    isValid: function(creditCardNumber) {
+      console.log(creditCardNumber);
+    }
+  } 
+  export default validator;
+
+
+
+
+
+  
+  
+  
